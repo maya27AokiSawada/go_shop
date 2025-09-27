1e6a3f4c453a00dad0a8d4c900b0ada74c92d373
Maya Fatima Kanagae <fatima.sumomo@outlook.com>
2025-09-27 11:16:14 +0900

    25-09-27 11:15 models をfreezed化

--- lib/models/shopping_list.dart
+++ lib/models/shopping_list.dart
@@ -1,81 +1,48 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive/hive.dart';
+
+part 'shopping_list.freezed.dart';
 part 'shopping_list.g.dart';
 
 @HiveType(typeId: 3)
-class ShoppingItem {
-  @HiveField(0)
-  final String memberId;
-  @HiveField(1)
-  final String name; // 商品名
-  @HiveField(2)
-  final int quantity; // 数量
-  @HiveField(3)
-  final DateTime registeredDate; // 登録日
-  @HiveField(4)
-  final DateTime? purchaseDate; // 購入日
-  @HiveField(5)
-  final bool isPurchased; // true: 購入済み、 false: 未購入
-  @HiveField(6)
-  final int shoppingInterval; // 0:　繰り返し購入ではない　other:　繰り返し購入間隔（日数）
+@freezed
+class ShoppingItem with _$ShoppingItem {
+  const factory ShoppingItem({
+    @HiveField(0) required String memberId,
+    @HiveField(1) required String name, // 商品名
+    @HiveField(2) @Default(1) int quantity, // 数量
+    @HiveField(3) required DateTime registeredDate, // 登録日
+    @HiveField(4) DateTime? purchaseDate, // 購入日
+    @HiveField(5) @Default(false) bool isPurchased, // true: 購入済み、 false: 未購入
+    @HiveField(6) @Default(0) int shoppingInterval, // 0:　繰り返し購入ではない　other:　繰り返し購入間隔（日数）
+  }) = _ShoppingItem;
 
-  ShoppingItem({
-    required this.memberId,
-    required this.name,
-    this.quantity = 1,
-    DateTime? registeredDate,
-    DateTime? purchaseDate,
-    this.isPurchased = false,
-    required this.shoppingInterval,
-  }) : registeredDate = registeredDate ?? DateTime.now(),
-        purchaseDate = null;
-
-  ShoppingItem copyWith({
-    bool? isPurchased,
-    DateTime? purchaseDate,
-    bool? isEnableRepeat,
+  // ファクトリーコンストラクタでカスタムロジック
+  factory ShoppingItem.createNow({
+    required String memberId,
+    required String name,
+    int quantity = 1,
+    bool isPurchased = false,
+    int shoppingInterval = 0,
   }) {
     return ShoppingItem(
-      // ... 他のフィールドはthisから継承
       memberId: memberId,
       name: name,
       quantity: quantity,
-      registeredDate: registeredDate,
+      registeredDate: DateTime.now(),
+      isPurchased: isPurchased,
       shoppingInterval: shoppingInterval,
-      // 指定されたフィールドのみ新しい値に置き換え
-      isPurchased: isPurchased ?? false,
-      purchaseDate : isPurchased == true ? (purchaseDate ?? DateTime.now()) : null,
     );
-  }
-
-  // json_serializableで自動生成されるメソッド
-//  factory ShoppingItem.fromJson(Map<String, dynamic> json) => _$ShoppingItemFromJson(json);
-//  Map<String, dynamic> toJson() => _$ShoppingItemToJson(this);
-
-  @override
-  String toString() {
-    return 'ShoppingItem(memberId: $memberId, name: $name, quantity: $quantity, registeredDate: $registeredDate,'
-        ' purchaseDate: $purchaseDate, isPurchased: $isPurchased, shoppingInterval: $shoppingInterval)';
   }
 }
 
 @HiveType(typeId: 4)
-class ShoppingList {
-  @HiveField(0)
-  String ownerUid;
-  @HiveField(1)
-  String groupId;
-  @HiveField(2)
-  String groupName;
-  @HiveField(3)
-  final List<ShoppingItem> items;
-  ShoppingList({
-    required this.ownerUid,
-    required this.groupId,
-    required this.groupName,
-    this.items = const [],
-  });
-  @override
-  String toString() {
-    return 'ShoppingList(ownerUid: $ownerUid, groupId: $groupId, groupName: $groupName, items: $items)';
-  }
+@freezed
+class ShoppingList with _$ShoppingList {
+  const factory ShoppingList({
+    @HiveField(0) required String ownerUid,
+    @HiveField(1) required String groupId,
+    @HiveField(2) required String groupName,
+    @HiveField(3) @Default([]) List<ShoppingItem> items,
+  }) = _ShoppingList;
 }
